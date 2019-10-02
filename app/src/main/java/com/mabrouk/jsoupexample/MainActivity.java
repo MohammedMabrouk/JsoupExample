d277f66f7aa6217aa3de19b134dd968c4cff629d
MohammedMabrouk <mohamedmabrouk180@gmail.com>
2019-10-02 18:03:13 +0200

    slider fixes.

--- app/src/main/java/com/mabrouk/jsoupexample/MainActivity.java
+++ app/src/main/java/com/mabrouk/jsoupexample/MainActivity.java
@@ -1,34 +1,141 @@
 package com.mabrouk.jsoupexample;
 
+import android.content.Context;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.PagerSnapHelper;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SnapHelper;
 import android.util.Log;
+import android.view.View;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements GetSliderContentAsync.sliderImagesListener {
+
+    SliderAdapter sliderAdapter;
+    RecyclerView sliderRecyclerView, sliderRecyclerView2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //new Content().execute();
+        sliderRecyclerView = findViewById(R.id.slider_recycler_view);
+        sliderRecyclerView.setLayoutManager(new CenterZoomLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
+        sliderRecyclerView.setHasFixedSize(true);
 
-        // images directory
-        // MasterContentDiv WithBanner > MainContentDiv > MainPanel > Carousel > ul > li(s)
+        sliderRecyclerView2 = findViewById(R.id.slider_recycler_view2);
+        sliderRecyclerView2.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
+        sliderRecyclerView2.setHasFixedSize(true);
 
-        // image details
-        // src: ul > li.ContentText > div.container > h2.Title
+        new GetSliderContentAsync(this).execute();
 
-        new ShowDetailsAsync().execute();
+//        new getAllProgramsAsync().execute();
     }
 
-    private static class ShowDetailsAsync extends AsyncTask<Void, Void, Void> {
+    @Override
+    public void onSliderImagesFetched(final List<String> imagesUrlList, List<String> imagesTitlesList) {
+        sliderAdapter = new SliderAdapter(imagesUrlList, imagesTitlesList, this);
+        sliderRecyclerView.setAdapter(sliderAdapter);
+
+        sliderRecyclerView2.setAdapter(sliderAdapter);
+
+        SnapHelper snapHelper = new PagerSnapHelper();
+        snapHelper.attachToRecyclerView(sliderRecyclerView);
+//        carouselView = findViewById(R.id.carouselView);
+//        carouselView.setPageCount(imagesTitlesList.size());
+//        carouselView.setImageListener(new ImageListener() {
+//            @Override
+//            public void setImageForPosition(int i, ImageView imageView) {
+////                Picasso.get().load(imagesUrlList.get(i))
+//////                        .error(context.getResources().getDrawable(R.drawable.car_placeholder))
+////                        .into(imageView);
+//                imageView.setImageResource(R.drawable.ic_launcher_background);
+//            }
+//        });
+
+//        carouselView.setPageCount(imagesTitlesList.size());
+//        carouselView.setImageListener(imageListener);
+
+
+//        carouselView.setImageListener(imageListener);
+    }
+
+    public class CenterZoomLayoutManager extends LinearLayoutManager {
+
+        private final float mShrinkAmount = 0.15f;
+        private final float mShrinkDistance = 0.9f;
+
+        public CenterZoomLayoutManager(Context context) {
+            super(context);
+        }
+
+        public CenterZoomLayoutManager(Context context, int orientation, boolean reverseLayout) {
+            super(context, orientation, reverseLayout);
+        }
+
+        @Override
+        public int scrollVerticallyBy(int dy, RecyclerView.Recycler recycler, RecyclerView.State state) {
+            int orientation = getOrientation();
+            if (orientation == VERTICAL) {
+                int scrolled = super.scrollVerticallyBy(dy, recycler, state);
+                float midpoint = getHeight() / 2.f;
+                float d0 = 0.f;
+                float d1 = mShrinkDistance * midpoint;
+                float s0 = 1.f;
+                float s1 = 1.f - mShrinkAmount;
+                for (int i = 0; i < getChildCount(); i++) {
+                    View child = getChildAt(i);
+                    float childMidpoint =
+                            (getDecoratedBottom(child) + getDecoratedTop(child)) / 2.f;
+                    float d = Math.min(d1, Math.abs(midpoint - childMidpoint));
+                    float scale = s0 + (s1 - s0) * (d - d0) / (d1 - d0);
+                    child.setScaleX(scale);
+                    child.setScaleY(scale);
+                }
+                return scrolled;
+            } else {
+                return 0;
+            }
+        }
+
+        @Override
+        public int scrollHorizontallyBy(int dx, RecyclerView.Recycler recycler, RecyclerView.State state) {
+            int orientation = getOrientation();
+            if (orientation == HORIZONTAL) {
+                int scrolled = super.scrollHorizontallyBy(dx, recycler, state);
+
+                float midpoint = getWidth() / 2.f;
+                float d0 = 0.f;
+                float d1 = mShrinkDistance * midpoint;
+                float s0 = 1.f;
+                float s1 = 1.f - mShrinkAmount;
+                for (int i = 0; i < getChildCount(); i++) {
+                    View child = getChildAt(i);
+                    float childMidpoint =
+                            (getDecoratedRight(child) + getDecoratedLeft(child)) / 2.f;
+                    float d = Math.min(d1, Math.abs(midpoint - childMidpoint));
+                    float scale = s0 + (s1 - s0) * (d - d0) / (d1 - d0);
+                    child.setScaleX(scale);
+                    child.setScaleY(scale);
+                }
+                return scrolled;
+            } else {
+                return 0;
+            }
+
+        }
+    }
+
+    private static class getProgramDetailsAsync extends AsyncTask<Void, Void, Void> {
         Document document;
 
         @Override
@@ -40,8 +147,7 @@ public class MainActivity extends AppCompatActivity {
         protected Void doInBackground(Void... voids) {
 
             try {
-                document = Jsoup.connect("http://www.natgeotv.com/ae/animal-fight-club").get();
-
+                document = Jsoup.connect("http://www.natgeotv.com/ae/animal-fight-club/about").get();
             } catch (Exception e) {
                 e.printStackTrace();
             }
@@ -64,7 +170,7 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    private static class ShowsAsync extends AsyncTask<Void, Void, Void> {
+    private static class getAllProgramsAsync extends AsyncTask<Void, Void, Void> {
 
         String pageTitle = "";
         Elements showsContainers;
@@ -120,51 +226,4 @@ public class MainActivity extends AppCompatActivity {
             }
         }
     }
-
-    private static class Content extends AsyncTask<Void, Void, Void> {
-
-        String pageTitle = "";
-        Elements images;
-
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-        }
-
-        @Override
-        protected Void doInBackground(Void... voids) {
-
-            try {
-                //Connect to the website
-
-                Document document = Jsoup.connect("http://www.natgeotv.com/ae").get();
-
-                pageTitle = document.title();
-
-                images = document.select("div.MasterContentDiv.WithBanner > div.MainContentDiv > div.MainPanel > div.Carousel > ul > li");
-
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-            return null;
-        }
-
-        @Override
-        protected void onPostExecute(Void aVoid) {
-            super.onPostExecute(aVoid);
-
-            Log.v("tagg", pageTitle);
-            Log.v("tagg", images.size() + "");
-
-            for (Element e : images) {
-                // get image text
-                Log.v("tagg", e.select("ul > li.ContentText > div.container > h2.Title").text());
-                // get image link
-                Log.v("tagg", e.select("ul > li.MainPanelImage > a > img").attr("src"));
-                // get program link
-                Log.v("tagg", e.select("ul > li.MainPanelImage > a").attr("href"));
-            }
-        }
-    }
-
 }
---
+++ app/src/main/java/com/mabrouk/jsoupexample/SliderAdapter.java
@@ -0,0 +1,79 @@
+package com.mabrouk.jsoupexample;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+public class SliderAdapter extends RecyclerView.Adapter<SliderAdapter.SlideViewHolder> {
+
+    List<String> imagesUrlList;
+    List<String> imagesTitlesList;
+    Context context;
+
+
+    public SliderAdapter(List<String> imagesUrlList, List<String> imagesTitlesList, Context context) {
+        this.imagesUrlList = imagesUrlList;
+        this.imagesTitlesList = imagesTitlesList;
+        this.context = context;
+    }
+
+    @Override
+    public SlideViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view;
+        view = LayoutInflater.from(parent.getContext()).inflate(R.layout.card_slide, parent, false);
+        return new SliderAdapter.SlideViewHolder(view, context);
+    }
+
+    @Override
+    public void onBindViewHolder(SlideViewHolder holder, int position) {
+        //set title
+        holder.programTitleTextView.setText(imagesTitlesList.get(position));
+//        holder.rentalNumber.setText(String.valueOf(rentalsList.get(position).getRANumber()));
+
+        if (imagesUrlList.get(position) != null && !imagesUrlList.get(position).isEmpty()) {
+                Picasso.get().load(imagesUrlList.get(position))
+//                        .error(context.getResources().getDrawable(R.drawable.car_placeholder))
+                        .into(holder.programImageView);
+        }
+
+        holder.programTitle = imagesTitlesList.get(position);
+        holder.programImageUrl = imagesUrlList.get(position);
+    }
+
+    @Override
+    public int getItemCount() {
+        return imagesTitlesList.size();
+    }
+
+    class SlideViewHolder extends RecyclerView.ViewHolder {
+
+        String programImageUrl;
+        String programTitle;
+
+        TextView programTitleTextView;
+        ImageView programImageView;
+
+        SlideViewHolder(View itemView, final Context context) {
+            super(itemView);
+
+            programImageView = itemView.findViewById(R.id.program_image);
+            programTitleTextView = itemView.findViewById(R.id.program_title);
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(context, programTitle, Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+    }
+}
